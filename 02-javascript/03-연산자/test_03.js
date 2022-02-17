fee89b1af87f758ce00bd05ce00b0cec79be4951
doyookim <jeje_k@naver.com>
2022-02-17 21:24:51 +0900

    2022-02-17

--- 02-javascript/03-연산자/test_03.js
+++ 02-javascript/03-연산자/test_03.js
@@ -1,5 +1,6 @@
 const numOfApples = 123;
 let basket = numOfApples / 10 ;
+console.log(basket);
 if (basket >= 10)
 {
     ++basket 
@@ -12,15 +13,17 @@ console.groupEnd();
 
 // 사과의 수
 
-const numberOfApple = 123;
+const numOfApples = 123;
 
 // 10으로 나눈 나머지를 소수점 값으로 환산
 const extra = (numOfApples % 10) / 10;
 console.log(extra);
 
-// 첫번째 풀이
-// let basketCount = (numOfApples / 10) > 0 ?- extra + 1 : (numOfApples / 10) - extra;
-// console.log(basketCount);
+//첫번째 풀이
+let basketCount = numOfApples / 10;
+console.log(basketCount);
+let basketCount = extra > 0 ? (numOfApples / 10) - extra + 1 : (numOfApples / 10) - extra;
+console.log(basketCount);
 
 // 두번째 풀이
 let basketCount = (numOfApples / 10)  - extra;
